81fa15c316582b0088eb2cfaf78a6af9d2a8fd9c
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-01 23:04:47 -0400

    sum from a to b

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
+// eslint-disable-next-line func-style
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+// Sum all the values from fromN up to toN
+
+  if (fromN === toN) {
+    return toN;
+
+  } else {
+    return sum(fromN, toN - 1) + toN;
+  }
+
+
 }
+//console.log(sum(1,4));
+
 
 module.exports = sum;
